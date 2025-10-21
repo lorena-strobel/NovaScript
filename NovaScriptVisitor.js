f4c9701f9c4f5389e7b90a732fec43ff9558112c
Spacery <msa.mt314@gmail.com>
2025-10-21 19:08:03 -0400

    feat(main): Adiciona NovaScript visitor e tratamento de erros
    - Implementado NovaScriptVisitor para análise de árvore de traversal.
    - Adicionados scripts de exemplo demonstrando erros personalizados e sintaxe.
    - Criado main.js para tratamento de entrada de arquivos e lógica de análise.
    - Atualizado package.json para definir o tipo de módulo para importações ES6.

---
+++ NovaScriptVisitor.js
@@ -0,0 +1,178 @@
+// Generated from NovaScript.g4 by ANTLR 4.13.2
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete generic visitor for a parse tree produced by NovaScriptParser.
+
+export default class NovaScriptVisitor extends antlr4.tree.ParseTreeVisitor {
+
+	// Visit a parse tree produced by NovaScriptParser#programa.
+	visitPrograma(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#statement.
+	visitStatement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#lista.
+	visitLista(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#declaracao.
+	visitDeclaracao(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#lista_atrib.
+	visitLista_atrib(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#lista_id.
+	visitLista_id(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#ler.
+	visitLer(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#funcao_conversao.
+	visitFuncao_conversao(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#escrever.
+	visitEscrever(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#concatenacao_log.
+	visitConcatenacao_log(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#elemento_log.
+	visitElemento_log(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#expressao.
+	visitExpressao(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#expmat.
+	visitExpmat(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#termo.
+	visitTermo(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#fator.
+	visitFator(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#numero.
+	visitNumero(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#condicional.
+	visitCondicional(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#se.
+	visitSe(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#laco.
+	visitLaco(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#inicio.
+	visitInicio(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#final.
+	visitFinal(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#bloco.
+	visitBloco(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#atribuicao.
+	visitAtribuicao(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#condicao.
+	visitCondicao(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#condicao_or.
+	visitCondicao_or(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#condicao_and.
+	visitCondicao_and(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#condicao_not.
+	visitCondicao_not(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by NovaScriptParser#condicao_par.
+	visitCondicao_par(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+
+}
